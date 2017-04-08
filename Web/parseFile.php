a3cb72ecd16c6ebcd578fed6c8af39300b17e013
jschummy <jt.schumm@gmail.com>
2017-04-08 16:37:27 -0400

    I fudged up last time
     but here is the whole code plus fixes from james MISTAKES

--- Web/parseFile.php
+++ Web/parseFile.php
@@ -1,32 +1,70 @@
 <?php
-$txt_file    = file_get_contents('path/to/file.txt');
-$rows        = explode("\n", $txt_file);
-array_shift($rows);
+$user = "SA";
+$PW = "111#ZXC#222";
+$conn = new PDO("sqlsrv:server=cisvm-SenPro1;Database=BandsNearMe;ConnectionPooling=0", $user, $PW);
+// set the PDO error mode to exception
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+$target_file = file_get_contents($_FILES['fileToUpload']['tmp_name']);
+echo "<pre>";
+    print_r($_FILES);
+    echo file_get_contents($_FILES['file']['tmp_name']);
+    echo "</pre>";
+$rows        = explode(PHP_EOL, $target_file);
+
+//send request to Show table to retrieve the last show ID
+//array_shift($rows);
 foreach($rows as $row => $data)
 {
-	//hi rachel
-    //get row data
+echo "we're in";
+$sql = "SELECT TOP 1 * FROM Show ORDER BY ShowID DESC";
+$q = $conn->query($sql);
+$q -> execute();
+$result = $q->fetchAll();
+$lastId = $result[0][0];
+$lastId = str_replace('S', '', $lastId);
+$lastId = $lastId + 1;
+$idLength = strlen($lastId);
+$zeroAppend="0";
+for ($i =1; $i <(13 - $idLength); $i++)
+	{
+	$zeroAppend = $zeroAppend."0";
+	}
+$zeroAppend = $zeroAppend.$lastId;
+$showID = "S".$zeroAppend;
+echo "halp";
+//get row data
     $row_data = explode(',', $data);
+
+$sql = "INSERT INTO SHOW (ShowID, BUserName, VUserName, SDesc, ShowDate, ShowTime) VALUES ('$showID','$row_data[0]','$row_data[1]','$row_data[2]','$row_data[3]','$row_data[4]')";
+//$sql = "INSERT INTO SHOW (ShowID, BandName, VenueName, SDesc, ShowDate, ShowTime) VALUES ('S0000000000012','Willy Wonka','Chocolate Factory','Chocolate wasted', '2018-02-16', '03:30')";
+
+    
 
     $info[$row]['band']           = $row_data[0];
     $info[$row]['venue']         = $row_data[1];
     $info[$row]['description']  = $row_data[2];
-    $info[$row]['time']       = $row_data[3];
+    $info[$row]['date']       = $row_data[3];
+    $info[$row]['time']       = $row_data[4];
+    
 
     //display data
-    echo 'Row ' . $row . ' BAND: ' . $info[$row]['band'] . '<br />';
-    echo 'Row ' . $row . ' VENUE: ' . $info[$row]['venue'] . '<br />';
-    echo 'Row ' . $row . ' DESCRIPTION: ' . $info[$row]['description'] . '<br />';
-    echo 'Row ' . $row . ' TIME: ' . $info[$row]['time']  . '<br />';
-
-
-
-    foreach($row_images as $row_image)
+    echo "$showID</br>";
+    echo ' BAND: ' . $info[$row]['band'] . '<br />';
+    echo ' VENUE: ' . $info[$row]['venue'] . '<br />';
+    echo ' DESCRIPTION: ' . $info[$row]['description'] . '<br />';
+    echo ' DATE: ' . $info[$row]['date']  . '<br />';
+    echo ' TIME: ' . $info[$row]['time']  . '<br />';
+try{
+	$q = $conn->query($sql);
+       // $q -> execute();
+}
+catch(PDOException $e)
     {
-        echo ' - ' . $row_image . '<br />';
+    echo "$e";
     }
+    
 
-    echo '<br />';
+    echo 'TITZ';
 }
 ?>
