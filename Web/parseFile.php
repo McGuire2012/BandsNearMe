34ab24c24fa99a606adea03f83eb4b7ad71dd4a6
jschummy <jt.schumm@gmail.com>
2017-04-08 13:00:36 -0400

    Add files via upload

---
+++ Web/parseFile.php
@@ -0,0 +1,31 @@
+<?php
+$txt_file    = file_get_contents('path/to/file.txt');
+$rows        = explode("\n", $txt_file);
+array_shift($rows);
+
+foreach($rows as $row => $data)
+{
+    //get row data
+    $row_data = explode(',', $data);
+
+    $info[$row]['band']           = $row_data[0];
+    $info[$row]['venue']         = $row_data[1];
+    $info[$row]['description']  = $row_data[2];
+    $info[$row]['time']       = $row_data[3];
+
+    //display data
+    echo 'Row ' . $row . ' BAND: ' . $info[$row]['band'] . '<br />';
+    echo 'Row ' . $row . ' VENUE: ' . $info[$row]['venue'] . '<br />';
+    echo 'Row ' . $row . ' DESCRIPTION: ' . $info[$row]['description'] . '<br />';
+    echo 'Row ' . $row . ' TIME: ' . $info[$row]['time']  . '<br />';
+
+
+
+    foreach($row_images as $row_image)
+    {
+        echo ' - ' . $row_image . '<br />';
+    }
+
+    echo '<br />';
+}
+?>
