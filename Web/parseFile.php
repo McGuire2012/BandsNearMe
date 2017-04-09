a4b0e351cd63a5b5dfe9a608e07629d6735a5cfb
jschummy <jt.schumm@gmail.com>
2017-04-09 11:52:08 -0400

    Updated file upload parse for bulkdata import

--- Web/parseFile.php
+++ Web/parseFile.php
@@ -6,17 +6,12 @@ $conn = new PDO("sqlsrv:server=cisvm-SenPro1;Database=BandsNearMe;ConnectionPool
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $target_file = file_get_contents($_FILES['fileToUpload']['tmp_name']);
-echo "<pre>";
-    print_r($_FILES);
-    echo file_get_contents($_FILES['file']['tmp_name']);
-    echo "</pre>";
+
 $rows        = explode(PHP_EOL, $target_file);
 
 //send request to Show table to retrieve the last show ID
-//array_shift($rows);
 foreach($rows as $row => $data)
 {
-echo "we're in";
 $sql = "SELECT TOP 1 * FROM Show ORDER BY ShowID DESC";
 $q = $conn->query($sql);
 $q -> execute();
@@ -32,39 +27,22 @@ for ($i =1; $i <(13 - $idLength); $i++)
 	}
 $zeroAppend = $zeroAppend.$lastId;
 $showID = "S".$zeroAppend;
-echo "halp";
 //get row data
     $row_data = explode(',', $data);
 
 $sql = "INSERT INTO SHOW (ShowID, BUserName, VUserName, SDesc, ShowDate, ShowTime) VALUES ('$showID','$row_data[0]','$row_data[1]','$row_data[2]','$row_data[3]','$row_data[4]')";
-//$sql = "INSERT INTO SHOW (ShowID, BandName, VenueName, SDesc, ShowDate, ShowTime) VALUES ('S0000000000012','Willy Wonka','Chocolate Factory','Chocolate wasted', '2018-02-16', '03:30')";
 
-    
-
-    $info[$row]['band']           = $row_data[0];
-    $info[$row]['venue']         = $row_data[1];
-    $info[$row]['description']  = $row_data[2];
-    $info[$row]['date']       = $row_data[3];
-    $info[$row]['time']       = $row_data[4];
-    
-
-    //display data
-    echo "$showID</br>";
-    echo ' BAND: ' . $info[$row]['band'] . '<br />';
-    echo ' VENUE: ' . $info[$row]['venue'] . '<br />';
-    echo ' DESCRIPTION: ' . $info[$row]['description'] . '<br />';
-    echo ' DATE: ' . $info[$row]['date']  . '<br />';
-    echo ' TIME: ' . $info[$row]['time']  . '<br />';
+ 
 try{
 	$q = $conn->query($sql);
-       // $q -> execute();
+	echo "$row_data[0], $row_data[1], $row_data[2], $row_data[3], $row_data[4]  has been created with show ID: $showID </br>";
+    
 }
 catch(PDOException $e)
     {
-    echo "$e";
+    echo "$row_data[0], $row_data[1], $row_data[2], $row_data[3], $row_data[4] was not created </br>";
     }
     
 
-    echo 'TITZ';
 }
 ?>
