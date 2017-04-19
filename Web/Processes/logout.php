069c3ba432cfea9b446c269f1804ee23619a073b
jschummy <jt.schumm@gmail.com>
2017-04-19 17:16:53 -0400

    ANOTHER FUCKING LINK FIX

--- Web/Processes/logout.php
+++ Web/Processes/logout.php
@@ -3,7 +3,7 @@ session_start();
 unset($_SESSION['email']);
  unset($_SESSION['passwords']);
  session_destroy();
-		header("Location: index.php");
+		header("Location: ../index.php");
 		die();
 
   ?>
